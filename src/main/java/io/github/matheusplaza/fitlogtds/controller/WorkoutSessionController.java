dbd86c0f3fd6b605429e77f709bce8ae7980013c
Matheus Plaza <plazachave.mp7@gmail.com>
2025-09-14 19:37:03 -0300

    feat(session): Define os endpoints RESTful para o recurso de WorkoutSession

--- src/main/java/io/github/matheusplaza/fitlogtds/controller/WorkoutSessionController.java
+++ src/main/java/io/github/matheusplaza/fitlogtds/controller/WorkoutSessionController.java
@@ -1,10 +1,47 @@
 package io.github.matheusplaza.fitlogtds.controller;
 
+import io.github.matheusplaza.fitlogtds.controller.dto.WorkoutSessionCreateDTO;
+import io.github.matheusplaza.fitlogtds.controller.dto.WorkoutSessionDTO;
+import io.github.matheusplaza.fitlogtds.service.WorkoutSessionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/sessions")
 public class WorkoutSessionController implements GenericController {
+    private final WorkoutSessionService service;
 
+    @PostMapping
+    public ResponseEntity<WorkoutSessionDTO> saveSession(@RequestBody @Valid WorkoutSessionCreateDTO dto) {
+        WorkoutSessionDTO savedSession = service.saveSession(dto);
+        URI location = gerarHeadLocation(savedSession.id());
+        return ResponseEntity.created(location).body(savedSession);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<WorkoutSessionDTO>> getListSessions() {
+        return ResponseEntity.ok(service.getListSessions());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WorkoutSessionDTO> getSession(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getSession(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<WorkoutSessionDTO> updateSession(@PathVariable Long id, @RequestBody @Valid  WorkoutSessionCreateDTO dto) {
+        return ResponseEntity.ok(service.updateSession(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSession(@PathVariable Long id) {
+        service.deleteSession(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/io/github/matheusplaza/fitlogtds/service/WorkoutSessionService.java
+++ src/main/java/io/github/matheusplaza/fitlogtds/service/WorkoutSessionService.java
@@ -1,4 +1,27 @@
 package io.github.matheusplaza.fitlogtds.service;
 
+import io.github.matheusplaza.fitlogtds.controller.dto.WorkoutSessionCreateDTO;
+import io.github.matheusplaza.fitlogtds.controller.dto.WorkoutSessionDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class WorkoutSessionService {
+    
+    public WorkoutSessionDTO saveSession(WorkoutSessionCreateDTO dto) {
+    }
+
+    public List<WorkoutSessionDTO> getListSessions() {
+    }
+
+    public WorkoutSessionDTO getSession(Long id) {
+    }
+
+    public WorkoutSessionDTO updateSession(Long id, WorkoutSessionCreateDTO dto) {
+    }
+
+    public void deleteSession(Long id) {
+
+    }
 }

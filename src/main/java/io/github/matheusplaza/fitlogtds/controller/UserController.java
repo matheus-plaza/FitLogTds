c253e321932ede7f70604ebcb3fab7f1f55b9bfe
Matheus Plaza <plazachave.mp7@gmail.com>
2025-09-18 22:35:10 -0300

    feat(user): Implementa arquitetura de crud de usuarios

--- src/main/java/io/github/matheusplaza/fitlogtds/controller/UserController.java
+++ src/main/java/io/github/matheusplaza/fitlogtds/controller/UserController.java
@@ -1,4 +1,48 @@
 package io.github.matheusplaza.fitlogtds.controller;
 
-public class UserController {
+import io.github.matheusplaza.fitlogtds.controller.dto.UserCreateDTO;
+import io.github.matheusplaza.fitlogtds.controller.dto.UserDTO;
+import io.github.matheusplaza.fitlogtds.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("users")
+public class UserController implements GenericController{
+
+    private final UserService service;
+
+    @PostMapping
+    public ResponseEntity<UserDTO> saveUser(@RequestBody @Valid UserCreateDTO dto) {
+        UserDTO user = service.saveUser(dto);
+        URI location = gerarHeadLocation(user.id());
+        return ResponseEntity.created(location).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getUser(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        return ResponseEntity.ok(service.getListUsers());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO dto) {
+        return  ResponseEntity.ok(service.updateUser(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        service.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/io/github/matheusplaza/fitlogtds/service/UserService.java
+++ src/main/java/io/github/matheusplaza/fitlogtds/service/UserService.java
@@ -1,16 +1,67 @@
 package io.github.matheusplaza.fitlogtds.service;
 
+import io.github.matheusplaza.fitlogtds.controller.dto.UserCreateDTO;
+import io.github.matheusplaza.fitlogtds.controller.dto.UserDTO;
+import io.github.matheusplaza.fitlogtds.exceptions.DuplicateResourceException;
 import io.github.matheusplaza.fitlogtds.exceptions.NotFoundException;
+import io.github.matheusplaza.fitlogtds.mapper.UserMapper;
 import io.github.matheusplaza.fitlogtds.model.User;
 import io.github.matheusplaza.fitlogtds.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
+    private final UserRepository repository;
+    private final UserMapper mapper;
+
     public User findById(Long id) {
-        return userRepository.findById(id).orElseThrow(() -> new NotFoundException("Usuario nao encontrado"));
+        return repository.findById(id).orElseThrow(() -> new NotFoundException("Usuario nao encontrado"));
+    }
+
+    //TODO: No checkpoint de Seguranca irei implementar o hash da senha com PasswordEncoder antes de persistir
+    @Transactional
+    public UserDTO saveUser(UserCreateDTO userDTO) {
+        if(repository.existsByEmail(userDTO.email())){
+            throw new DuplicateResourceException("Email ja cadastrado");
+        }
+        User user = new User();
+        user.setEmail(userDTO.email());
+        user.setName(userDTO.name());
+        user.setPassword(userDTO.password());
+        return mapper.toDTO(repository.save(user));
+    }
+
+    public UserDTO getUser(Long id) {
+        return mapper.toDTO(findById(id));
+    }
+
+    public List<UserDTO> getListUsers() {
+        return repository.findAll().stream().map(mapper::toDTO).toList();
+    }
+
+    @Transactional
+    public UserDTO updateUser(Long id, UserDTO userDTO) {
+        User user = findById(id);
+        Optional.ofNullable(userDTO.name()).ifPresent(user::setName);
+        if (userDTO.email() != null) {
+            Optional<User> email = repository.findByEmail(userDTO.email());
+            if (email.isPresent() && !user.getId().equals(email.get().getId())) {
+                throw new DuplicateResourceException("Email ja cadastrado");
+            }
+            user.setEmail(userDTO.email());
+        }
+        return mapper.toDTO(repository.save(user));
+    }
+
+    public void deleteUser(Long id) {
+        User user = findById(id);
+        repository.delete(user);
     }
 }

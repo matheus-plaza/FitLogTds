dadfdb2b14baf60be3fe9557ad1010d6be0f9a2a
Matheus Plaza <plazachave.mp7@gmail.com>
2025-10-21 16:44:36 -0300

    refactor(exceptions): Padroniza respostas de erro para usar ErrorAPI

--- src/main/java/io/github/matheusplaza/fitlogtds/exceptions/ResourceExceptionHandler.java
+++ src/main/java/io/github/matheusplaza/fitlogtds/exceptions/ResourceExceptionHandler.java
@@ -1,29 +1,26 @@
 package io.github.matheusplaza.fitlogtds.exceptions;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Object> resourceNotFound(NotFoundException e, HttpServletRequest request) {
-        StandardError err = new StandardError(Instant.now(), HttpStatus.NOT_FOUND.value(), e.getMessage(), request.getRequestURI());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(err);
+    public ResponseEntity<ErrorAPI> resourceNotFound(NotFoundException e) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorAPI err = new ErrorAPI(status.value(), e.getMessage());
+        return ResponseEntity.status(status).body(err);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> methodArgumentNotValid(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorAPI> methodArgumentNotValid(MethodArgumentNotValidException e) {
         List<FieldError> fieldErrors = e.getFieldErrors();
         List<FieldMessage> fieldMessages = fieldErrors.stream().map(fe ->
                 new FieldMessage(fe.getField(), fe.getDefaultMessage())).toList();
@@ -32,7 +29,7 @@ public class ResourceExceptionHandler {
     }
 
     @ExceptionHandler(DuplicateResourceException.class)
-    public ResponseEntity<Object>  duplicateResourceException(DuplicateResourceException e) {
+    public ResponseEntity<ErrorAPI>  duplicateResourceException(DuplicateResourceException e) {
         ErrorAPI err = new ErrorAPI(HttpStatus.UNPROCESSABLE_ENTITY.value(), e.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT.value()).body(err);
     }
